52dce5c3e3f68fccd5adc7f99b5858f1754acc25
Andy <amillion3@gmail.com>
2018-04-10 20:12:44 -0500

    added some JS files

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('hello');
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+console.log('Adding: ', add(1,2));
+console.log('Subtracting: ', subtract(2,1));
+console.log('Multiply',multiply(10,10));
+console.log('Divide', divide(10,2));
